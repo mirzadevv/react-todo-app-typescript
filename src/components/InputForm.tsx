f10a685d28a4177e17aa4c332e210272e05527b7
mirzaei.dev <mirzaei.devv@gmail.com>
2021-11-15 22:29:42 +0330

    feat: add handleAdd function
    This feature helps to add a todo to the todos state, this feature was added into the parent component and was sent as a prop to it's child (lift state up will be happened)

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,12 +1,33 @@
-const InputForm = () => {
+import { ChangeEvent, useState } from "react";
+interface InputFormProps {
+  onAdd: (todoValue: string | number) => void;
+}
+
+const InputForm: React.FC<InputFormProps> = ({ onAdd }) => {
+  const [toodValue, setTodoValue] = useState<string | number>("");
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value);
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    onAdd(toodValue);
+    setTodoValue("");
+  };
+
   return (
     <div className="add-items d-flex">
       <input
         type="text"
         className="form-control todo-list-input"
         placeholder="What do you need to do today?"
+        onChange={handleChange}
+        value={toodValue}
       />
-      <button className="add btn btn-primary font-weight-bold todo-list-add-btn">
+      <button
+        className="add btn btn-primary font-weight-bold todo-list-add-btn"
+        onClick={handleSubmit}
+      >
         Add
       </button>
     </div>
--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,7 +1,30 @@
 import "./TodoApp.css";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import InputForm from "./InputForm";
 import TodosList from "./TodosList";
+
+interface todoObject {
+  id: string;
+  title: string | number;
+  isCompleted: boolean;
+}
+
 const TodoApp = () => {
+  const [todos, setTodos] = useState<todoObject[]>([]);
+
+  const handleAdd = (todoValue: string | number) => {
+    console.log(todoValue);
+    const newTodo: todoObject = {
+      id: uuidv4(),
+      title: todoValue,
+      isCompleted: false,
+    };
+    const cloudTodos = [...todos];
+    cloudTodos.push(newTodo);
+    setTodos(cloudTodos);
+  };
+
   return (
     <div className="page-content page-container" id="page-content">
       <div className="row container d-flex justify-content-center">
@@ -9,7 +32,7 @@ const TodoApp = () => {
           <div className="card px-3">
             <div className="card-body">
               <h4 className="card-title">Awesome Todo list</h4>
-              <InputForm />
+              <InputForm onAdd={handleAdd} />
               <TodosList />
             </div>
           </div>
